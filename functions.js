da3d553faa142af1f2b2c21cb87063f522d0269d
Yan <yanfornimedeiros@gmail.com>
2022-07-22 09:40:30 -0300

    Criando functions.js

---
+++ functions.js
@@ -0,0 +1,8 @@
+//declarando uma função
+function createPhrases() {
+    console.log('Estudar é muito bom');
+    console.log('Paciência e persistência');
+    console.log('Revisão é a mãe do aprendizado');
+}
+//executando uma função
+createPhrases()
