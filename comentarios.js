537db5024ebcd9f66280e21c6263a58836cb35f7
Yan <yanfornimedeiros@gmail.com>
2022-07-22 09:09:03 -0300

    Criando comentarios.js

---
+++ comentarios.js
@@ -0,0 +1,48 @@
+//este é um comentário
+/*
+    este é um comentário em bloco
+*/
+
+
+console.log('Esta função escreve uma mensagem no console do DevTools')
+
+console.log(` as crases permitem utilizar ' " ' " aspas simples e duplas livremente na mensagem 
+    além de permitir quebra de linhas
+`)
+
+//para utilizar números basta escreve-los ou usar a expressão matemática, é possível obter resultados como NaN (Not a Number) ou Infinity (que quase não aparece)
+console.log(5+5.13)
+
+//há o tipo de dado boolean que funciona como em outras liguagens
+console.log(5>3)
+console.log(50===500)
+
+/*
+Há o tipo undefined (indefinido) e null (nulo) 
+*/
+
+//os objetos possuem atributos(propriedades) e metodos(funcionalidades)     
+console.log({
+    nome: "Yan",
+    idade: 21,
+    coffee: function() {
+        console.log("☕")
+    }
+})
+
+
+// const teste = {
+//     nome: "Yan",
+//     idade: 21,
+//     coffee: function() {
+//         console.log("☕")
+//     }
+// }
+// console.log
+
+
+//array(lista ou vetores)
+console.log([
+    "Yan",
+    21
+])
