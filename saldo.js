88088749210f862e365b402c90249b9bba179f8c
Yan <yanfornimedeiros@gmail.com>
2022-07-25 08:18:46 -0300

    Adicionando solução de saldo.js

--- saldo.js
+++ saldo.js
@@ -1,5 +1,5 @@
 let family = {
-    incomes: [1000, 700, 800, 900], 
+    incomes: [0, 700, 800, 900], 
     expenses: [500, 800, 500, 600]
 }
 
@@ -7,18 +7,19 @@ function sumArray(array) {
     let total = 0;
     for (const value of array) {
         total += value;
+        //console.log(total);
     }
     return total
 }
 
 function balance() {
-    let incomes = family.incomes
-    let expenses = family.expenses
-    let balance = incomes - expenses
+    let calcIncomes = sumArray(family.incomes)
+    let calcExpenses = sumArray(family.expenses)
+    let balance = calcIncomes - calcExpenses
     console.log(balance);
     if (balance >= 0) {
-        console.log('Saldo positivo');
+        console.log(`Saldo positivo, seu saldo é: R$ ${balance}`);
     } else {
-        console.log('Saldo negativo');
+        console.log(`Saldo negativo, seu saldo é: R$ ${balance}`);
     }
 }
