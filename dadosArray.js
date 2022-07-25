c67348c9fa476b8e361cb20ac959d72e3a5a0542
Yan <yanfornimedeiros@gmail.com>
2022-07-25 11:01:28 -0300

    Adicionando a solução de dadosArray.js

--- dadosArray.js
+++ dadosArray.js
@@ -1,5 +1,4 @@
 const booksByCategory = [
-
     {
     category: "Riqueza",
     books: [
@@ -35,3 +34,52 @@ const booksByCategory = [
     ],
     }
 ]
+categories = booksByCategory.length
+
+console.log(`A quantidade de categorias é ${categories}`);
+
+for ( let i = 0; i < categories; i++) {
+    
+    let currentCategory = (booksByCategory[i].category)
+    let currentBooks = ((booksByCategory[i].books).length)
+    console.log(`Na categoria ${currentCategory}, há ${currentBooks} livros`);
+}
+
+let authors = []
+
+for (let l = 0; l < categories; l++) {
+
+    var totalAuthors = 0
+    let currentBooks = ((booksByCategory[l].books).length)
+    totalAuthors += currentBooks
+
+    for (let j = 0; j < totalAuthors; j++) {
+
+        let authorIn = authors.includes((booksByCategory[l].books[j]).author)
+        if (authorIn == false) {
+            authors.push((booksByCategory[l].books[j]).author)
+        }   
+    }
+}
+
+console.log(`São ${authors.length} autores no total`);
+
+function booksOf(author) {
+
+    let authorBooks = []
+    for (let k = 0; k <categories; k++) {
+         
+        for (let m = 0; m < totalAuthors; m++) {
+    
+            if (booksByCategory[k].books[m].author == author) {
+                authorBooks.push(((booksByCategory[k]).books[m]).title)
+            }
+        }
+    }
+    return(`Deste autor há ${authorBooks.length} livros, que são os seguintes: ${authorBooks}`);
+}
+
+
+
+
+
