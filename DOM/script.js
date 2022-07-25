5236414b90896e48e1e3ed6a6b36a23d0176d8e0
Yan <yanfornimedeiros@gmail.com>
2022-07-25 11:51:20 -0300

    Adicionando estudos em script.js

---
+++ DOM/script.js
@@ -0,0 +1,14 @@
+//pegar elementos pela classe
+const element = document.getElementsByClassName('one')
+console.log(element);
+
+// //pegar elementos pela tag
+const elementTag = document.getElementsByTagName('meta')
+console.log(elementTag);
+
+//querySelector
+const elementOne = document.querySelector('.one')
+
+//usar preferencialmente, pois funciona algumas funções a mais 
+//querySelectorAll
+const elementAll = document.querySelectorAll('.one')
