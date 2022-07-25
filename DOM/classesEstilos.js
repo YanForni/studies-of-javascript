3b1a6a38c5303662139bec26ecc334a24a483d01
Yan <yanfornimedeiros@gmail.com>
2022-07-25 12:32:13 -0300

    Adicionando estudos classesEstilos.js

---
+++ DOM/classesEstilos.js
@@ -0,0 +1,8 @@
+//style
+const element = document.querySelector('h1')
+//element.style.backgroundColor = 'rgba(0, 180, 200)';
+
+//classList
+element.classList.add('active');
+element.classList.remove('active');
+element.classList.toggle('active', 'green');
