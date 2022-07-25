697a7d406efcc34fe69a2a054a6f2ef6a8eb7fb5
YanForni <109802374+YanForni@users.noreply.github.com>
2022-07-25 11:22:08 -0300

    Revert "Exs i"

--- notas.js
+++ notas.js
@@ -1,25 +1,19 @@
+let score = 90
 
-function getScore(score) {
-    
-    let scoreA = (score>= 90 && score <= 100)
-    let scoreB = (score>= 80 && score <90)
-    let scoreC = (score>= 70 && score <80)
-    let scoreD = (score>= 60 && score <70)
-    let scoreF = (score < 60 && score > 0)
-    if (scoreA) {
-        scoreFinal = 'A';
-    } else if (scoreB) {
-        scoreFinal = 'B';
-    } else if (scoreC) {
-        scoreFinal = 'C';
-    } else if (scoreD) {
-        scoreFinal = 'D';
-    } else if (scoreF) {
-        scoreFinal = 'F';
-    } else {
-        scoreFinal = 'Nota inválida';
-    }
-
-    return scoreFinal
+let scoreA = (score>= 90)
+let scoreB = (score>= 80 && score <90)
+let scoreC = (score>= 70 && score <80)
+let scoreD = (score>= 60 && score <70)
+let scoreF = (score < 60)
+if (scoreA) {
+    console.log('A');
+} else if (scoreB) {
+    console.log('B');
+} else if (scoreC) {
+    console.log('C');
+} else if (scoreD) {
+    console.log('D');
+} else {
+    console.log('F')
 }
 
