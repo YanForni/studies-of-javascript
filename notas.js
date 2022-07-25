a4f5f1910424b4a65f41b2238d57f7b9d30ee62f
Yan <yanfornimedeiros@gmail.com>
2022-07-25 08:01:23 -0300

    Adicionando estudos em notas.js

--- notas.js
+++ notas.js
@@ -1,19 +1,25 @@
-let score = 90
 
-let scoreA = (score>= 90)
-let scoreB = (score>= 80 && score <90)
-let scoreC = (score>= 70 && score <80)
-let scoreD = (score>= 60 && score <70)
-let scoreF = (score < 60)
-if (scoreA) {
-    console.log('A');
-} else if (scoreB) {
-    console.log('B');
-} else if (scoreC) {
-    console.log('C');
-} else if (scoreD) {
-    console.log('D');
-} else {
-    console.log('F')
+function getScore(score) {
+    
+    let scoreA = (score>= 90 && score <= 100)
+    let scoreB = (score>= 80 && score <90)
+    let scoreC = (score>= 70 && score <80)
+    let scoreD = (score>= 60 && score <70)
+    let scoreF = (score < 60 && score > 0)
+    if (scoreA) {
+        scoreFinal = 'A';
+    } else if (scoreB) {
+        scoreFinal = 'B';
+    } else if (scoreC) {
+        scoreFinal = 'C';
+    } else if (scoreD) {
+        scoreFinal = 'D';
+    } else if (scoreF) {
+        scoreFinal = 'F';
+    } else {
+        scoreFinal = 'Nota inválida';
+    }
+
+    return scoreFinal
 }
 
