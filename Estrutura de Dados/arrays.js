22cff17fec28985a0119999edcbc048a8abc0047
Yan <yanfornimedeiros@gmail.com>
2022-08-01 14:00:55 -0300

    Prettier

--- Estrutura de Dados/arrays.js
+++ Estrutura de Dados/arrays.js
@@ -6,17 +6,17 @@ console.log(pilotos[3]); //Hamilton
 console.log(pilotos.length); //4
 
 for (let piloto of pilotos) {
-    console.log(piloto);
+  console.log(piloto);
 }
 
 //adiciona elemento
-pilotos.push('Alonso')
+pilotos.push("Alonso");
 console.log(pilotos);
 
 //encontra elemento
-const senna = pilotos.find(piloto => piloto ==='Senna')
+const senna = pilotos.find((piloto) => piloto === "Senna");
 console.log(senna);
 
 //removendo um elemento
-pilotos.splice(0,1)
+pilotos.splice(0, 1);
 console.log(pilotos);
