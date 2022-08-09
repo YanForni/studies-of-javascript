d6431d1d62cfe4c305817498cd7ad7b027a88ef9
Yan <yanfornimedeiros@gmail.com>
2022-08-09 14:01:02 -0300

    Utilizando fetch

---
+++ Assíncrono/fetch.js
@@ -0,0 +1,8 @@
+fetch("https://api.github.com/users/YanForni")
+  .then((response) => response.json())
+  .then((data) => {
+    fetch(data.repos_url)
+      .then((res) => res.json())
+      .then((d) => console.log(d))
+      .catch((erro) => console.log(erro));
+  });
