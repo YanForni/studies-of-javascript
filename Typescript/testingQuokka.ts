2bd9e86aa2a2780c79aa5b887d405c0859da1eda
Yan <yanfornimedeiros@gmail.com>
2022-08-15 09:31:18 -0300

    Primeira função em typescript

---
+++ Typescript/testingQuokka.ts
@@ -0,0 +1,14 @@
+function sum(num1: number, num2: number) {
+  return num1 + num2;
+}
+
+console.log(sum(1, 3));
+const hello = "hello world";
+console.log(hello.toUpperCase());
+
+const user = {
+  name: "Yan Forni de Medeiros",
+  email: "yanfornimedeiros@gmail.com",
+};
+
+console.log(user.name);
